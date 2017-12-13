7b83e00664c3bb7e9b0c87715c7c0c117309b684
Sarmiento <sarmife@prd.manulifeusa.com>
2017-12-13 09:59:56 -0500

    Setter Injection Demo

--- src/main/java/org/mahikero01/service/CustomerServiceImpl.java
+++ src/main/java/org/mahikero01/service/CustomerServiceImpl.java
@@ -8,13 +8,14 @@ import org.mahikero01.repository.HibernateCustomerRepositoryImpl;
 
 public class CustomerServiceImpl implements CustomerService {
 
-	private CustomerRepository customerRepository = new HibernateCustomerRepositoryImpl();
-	
-	/* (non-Javadoc)
-	 * @see org.mahikero01.service.CustomerService#findAll()
-	 */
+	private CustomerRepository customerRepository;
+
 	@Override
 	public List<Customer> findAll() {
 		return customerRepository.findAll();
+	}
+
+	public void setCustomerRepository(CustomerRepository customerRepository) {
+		this.customerRepository = customerRepository;
 	}
 }
